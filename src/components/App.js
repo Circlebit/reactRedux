ca2454a148aff75a2356c6d91b314eb84143e6b6
Paul Seibel <paul.seibel@mailbox.org>
2019-04-12 15:33:44 +0200

    add CoursesPage

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
+import CoursePage from "./courses/CoursesPage";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={AboutPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursePage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
